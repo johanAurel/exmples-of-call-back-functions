e5043cd7b93e33f3da175653179f6875d7d9694c
Johan Aurel Botouli <aurel.botouli@gmail.com>
2024-01-30 11:21:36 +0000

    first question

--- challenges/1-fetch-cats-by-owner.js
+++ challenges/1-fetch-cats-by-owner.js
@@ -1,5 +1,21 @@
+const { owners } = require("../utils/database");
 const request = require("../utils/request");
 
-const fetchCatsByOwner = (owner) => {};
+const fetchCatsByOwner = (owner) => {
+    console.log(owner)
+    return new Promise((resolve, reject)=>{
+    request(`/owners/${owner}/cats`,(err, cats)=>{
+        console.log(owners)
+        if(err){
+        reject(`404 - ${owner} not found`)
+        }
+        if(cats){ 
+            console.log(cats)
+           resolve(cats)
+        }
+
+    })
+})
+};
 
 module.exports = fetchCatsByOwner;
