2888185b635d4557a132352aa423e3f1aaccea0e
Johan Aurel Botouli <aurel.botouli@gmail.com>
2024-01-31 10:50:18 +0000

    finished number 8

--- challenges/8-compare-combine.js
+++ challenges/8-compare-combine.js
@@ -1,18 +1,31 @@
-const fs = require('fs/promises');
+const { error } = require("console");
+const fs = require("fs/promises");
 
+function findLongest() {
+  Promise.all([
+    fs.readFile("./super-secret-message.txt", "utf8"),
+    fs.readFile("./secret-message.txt", "utf8"),
+  ])
+    .then(([data1, data2]) => {
+      
+      if (data1.length > data2.length) {
+      console.log(`the file ${('./super-secret-message.txt')} is ${data1.length - data2.length} 
+      longer than ${('./secret-message.txt')}`)
+       
+    }
+      
+      else{
+      console.log(`the file ${('./secret-message.txt')} is ${data2.length - data1.length}
+       longer than ${('./super-secret-message.txt')}`)
+      
+    }
+    fs.writeFile(`./mega-secret-message.txt`, data1 + data2, 'utf-8')
 
-function findLongest(){
-const secretMessage = fs.readFile('./secret-message.txt');
-const superSecret =  fs.readFile('./super-secret-message.txt');
-
-console.log(secretMessage);
-console.log(superSecret);
-
-
-
-
-
-
-
-
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
+
+
+findLongest();
